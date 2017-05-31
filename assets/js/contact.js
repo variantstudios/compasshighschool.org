a08815c0aa581827be64602d55ce67e8bf2b584c
Variant Studios <team@variantstudios.com>
2017-05-31 02:22:04 +0000

    Updated 'assets/js/contact.js' via CloudCannon

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -5,16 +5,13 @@ $(document).ready(function() {
     var varArray = queryString.split("&");
     for (var i = 0; i < varArray.length; i++) {
       var param = varArray[i].split("=");
-      //console.log('param', param[1]);
       return param[1];
     }
   };
 
   
   if ($('body.contact').length > 0) {
-      console.log("contact page");
     if (queryString() == "true") {
-        console.log("true");
       $('#contact-message').html("<span><h3>Thank you. Your message has been sent. We will contact you shortly.</h3></span>");
     }
   };
