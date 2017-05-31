dacc6ef9e2cf9e8692b78780ba6039ef0a04bdc6
Variant Studios <team@variantstudios.com>
2017-05-31 02:19:55 +0000

    Updated 'assets/js/contact.js' via CloudCannon

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -12,6 +12,7 @@ $(document).ready(function() {
 
   
   if ($('body.contact').length > 0) {
+      console.log("contact page");
     if (queryString() == "yes") {
         console.log("test: " + queryString());
       $('#contact-message').html("<span><h3>Thank you. Your message has been sent. We will contact you shortly.</h3></span>");
