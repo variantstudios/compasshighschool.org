16a00a2bfe317942a132e445ad3cfd713d5e6404
Variant Studios <team@variantstudios.com>
2017-05-31 02:03:32 +0000

    Updated 'assets/js/contact.js' via CloudCannon

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,63 +1,18 @@
-var widgetId1;
-var onloadCallback = function() {
-       var widgetId1 = grecaptcha.render('recaptcha', {
-          'sitekey' : '6LfxPSAUAAAAAHG-l97Z0Gu8nxRifPt9yTRPT-IQ'
-        });
-      };
-      
-// Helper function to get form data in the supported format
-function getFormDataString(formEl) {
-  var formData = new FormData(formEl),
-      data = [];
+$(document).ready(function() {
 
-  for (var keyValue of formData) {
-    data.push(encodeURIComponent(keyValue[0]) + "=" + encodeURIComponent(keyValue[1]));
-  }
-
-  return data.join("&");
-}
-
-// Fetch the form element
-var formEl = document.getElementById("contact-form");
-
-// Override the submit event
-formEl.addEventListener("submit", function (e) {
-  e.preventDefault();
-  console.log("submitted");
-    
-  if (grecaptcha) {
-    var recaptchaResponse = grecaptcha.getResponse();
-    if (!recaptchaResponse) { // reCAPTCHA not clicked yet
-      return false;
+  function queryString() {
+    var queryString = window.location.search;
+    var varArray = queryString.split("&");
+    for (var i = 0; i < varArray.length; i++) {
+      var param = varArray[i].split("=");
+      console.log('param', param[1]);
+      return param[1];
     }
-  }
-
-  var request = new XMLHttpRequest();
-
-  request.addEventListener("load", function () {
-      console.log("status: " + request.status);
-    if (request.status === 200) { // CloudCannon redirects on success
-      // It worked
-      
-        formEl.reset(); //reset form
-        document.getElementById("status-message").innerHTML = "<p>Thanks for your message! We will be in touch soon.</p>";
-        //document.getElementById("recaptcha").innerHTML = "<div class=\"g-recaptcha\" data-sitekey=\"6LfB6RUUAAAAAEySJ7Fqc6dvL7oftcr1-l2ZANZA\"></div><br>";
-        //document.getElementById("recaptcha").innerHTML ="";
-        //onloadCallback();
-        //grecaptcha.reset();
-        //grecaptcha.reset(widgetId1);
-
-        console.log("sent");
-    } else {
-        // Didn't work
-        formEl.reset(); //reset form
-        grecaptcha.reset(); //reset google recaptcha
-        document.getElementById("status-message").innerHTML = "<p>Your submission has failed. Please try again.</p>";
-        console.log("failed");
+  };
+  
+  if ($('body.contact').length > 0) {
+    if (queryString() == "yes") {
+      $('#contact-message').html("<span><h3>Thank you. Your message has been sent. We will contact you shortly.</h3></span>");
     }
-  });
-
-  request.open(formEl.method, formEl.action);
-  request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  request.send(getFormDataString(formEl));
+  };
 });
