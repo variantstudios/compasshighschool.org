805f23d247fefd922f509e75457bd1205188db86
Brent Robbins <brent@variantstudios.com>
2018-04-27 16:08:36 -0600

    Initial sub page setup.

--- assets/js/dev/scripts.js
+++ assets/js/dev/scripts.js
@@ -32,19 +32,19 @@ $.getJSON('https://file-sajkowfnde.now.sh/compass-feed.json', function(data) {
   });
 });
 
-
 // YouTube Responsive Lightbox
 // Function to reveal lightbox and adding YouTube autoplay
-function revealVideo(div,video_id) {
+var revealVideo; // eslint-disable-line no-unused-vars
+revealVideo = function(div,video_id) {
   var video = document.getElementById(video_id).src;
   document.getElementById(video_id).src = video+'&autoplay=1'; // adding autoplay to the URL
   document.getElementById(div).style.display = 'block';
-}
-
+};
 // Hiding the lightbox and removing YouTube autoplay
-function hideVideo(div,video_id) {
+var hideVideo; // eslint-disable-line no-unused-vars
+hideVideo = function(div,video_id) {
   var video = document.getElementById(video_id).src;
   var cleaned = video.replace('&autoplay=1',''); // removing autoplay form url
   document.getElementById(video_id).src = cleaned;
   document.getElementById(div).style.display = 'none';
-}
+};
